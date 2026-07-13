c36800e5a6c4e328afd62e31e1a103c7e1c0f01e
raya <khasaatirayaa@gmail.com>
2026-07-14 00:46:59 +0300

    add authController login

---
+++ controllers/authController.js
@@ -0,0 +1,73 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+//generate jwt token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+//register
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    //check if user exists
+    const isExist = await User.findOne({ email });
+    if (isExist) {
+      return res.status(400).json({ message: "User already exist!" });
+    }
+
+    //not exist
+    //hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    //create user
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      token: generateToken(newUser._id),
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+//login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    //compare noe password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      return;
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
