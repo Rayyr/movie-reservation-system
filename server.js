2f86cc60a82b0dddc2f9a46af7adfcc87765f809
raya <khasaatirayaa@gmail.com>
2026-07-13 21:44:20 +0300

    make folder structure , db connection

---
+++ server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const port=process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log("hi");
+});
