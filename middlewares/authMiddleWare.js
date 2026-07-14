b7acb8e9c516fc148062a6eea98dfea39a8a73c8
raya <khasaatirayaa@gmail.com>
2026-07-14 19:14:20 +0300

    finish auth system

--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -10,7 +10,7 @@ export const protect = async (req, res, next) => {
 
     // 1. Check header
     if (req.headers.authorization?.startsWith("Bearer")) {
-      token = req.headers.authorization.split(" ")[1];
+      token = req.headers.authorization.split(" ")[1]; 
     }
 
     // 2. If no token
