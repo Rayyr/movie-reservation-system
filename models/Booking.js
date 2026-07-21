ac878a7978c4a71d041ca9a96409e595f323af97
raya <khasaatirayaa@gmail.com>
2026-07-21 13:30:03 +0300

    make booking model

---
+++ models/Booking.js
@@ -0,0 +1,34 @@
+import mongoose, { mongo } from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    showTime: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShowTime",
+      required: true,
+    },
+    seats: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seat",
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["BOOKED", "CANCELLED"],
+      default: "BOOKED",
+    },
+  },
+  { timestamps: true },
+);
+
+const Booking = new mongoose.model("Booking", bookingSchema);
+export default Booking;
