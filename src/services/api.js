da4e0aa9d0f1b281df9fe8395f753101613e4ccf
raya <khasaatirayaa@gmail.com>
2026-07-21 22:19:05 +0300

    set up axios api for request

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+//instance creator 
+const api=axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}`,
+});
+
+
+// attach token automatically
+api.interceptors.request.use((req)=>{
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return req;
+});
+
+
+export default api;
