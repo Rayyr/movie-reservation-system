735eceddca4e3aa1ac53c15d22a70ed064659045
raya <khasaatirayaa@gmail.com>
2026-07-27 21:09:07 +0300

    refactor expiryToken to get matched with authContext

--- src/utils/tokenExpiry.js
+++ src/utils/tokenExpiry.js
@@ -13,46 +13,22 @@ const getTokenExpiry = (token) => {
   }
 };
 
-const expireSession = () => {
-  //i will not invoke api logout since its protected route so since the token is expired so it will not enter it so i will take logout logic and put it here
-  clearTimeout(timerId);
-
-  localStorage.removeItem("user");
-
-  toast.error("Sorry , your session has been expired , log-in again", {
-    style: {
-      width: "500px",
-    },
-    onClose: () => {
-      window.location.replace("/login");
-    },
-  });
-};
-
-export const startTimer = (token) => {
+export const startTimer = (token, logout) => {
   //stop the timer(manage timer)
   clearTimeout(timerId);
 
   const timeLeft = getTokenExpiry(token) - Date.now();
 
   if (timeLeft <= 0) {
-    expireSession();
+    logout();
     return;
   }
 
   //it will assign a timer for -timeleft- once reaches 0 -expireSession will be called
-  timerId = setTimeout(expireSession, timeLeft);
+  timerId = setTimeout(() => {
+    toast.error("Sorry , your session has been expired , log-in again", {
+      style: { width: "500px" },
+    });
+    logout(); // 👈 delegate logout
+  }, timeLeft);
 };
-
-/* export const handleLogout = async () => {
-  clearTimeout(expiryTimer);
-
-  try {
-    await api.post("/api/auth/logout");
-  } catch {
-    // Logout locally even if the API/network fails.
-  } finally {
-    localStorage.removeItem("user");
-    window.location.replace("/login");
-  }
-}; */
