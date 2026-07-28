64fe27f9c5ebf47d61517ace1d3bd98a276774ef
raya <khasaatirayaa@gmail.com>
2026-07-28 16:02:49 +0300

    -add ui msgs in case of not allowed roles and in publicroute

--- src/App.js
+++ src/App.js
@@ -1,55 +1,110 @@
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminDashboard from './pages/AdminDashboard.jsx';
-import UserDashboard from './pages/UserDashboard.jsx';
-import Home from './pages/Home.jsx';
-import { ToastContainer ,Bounce} from 'react-toastify';
-import ForgotPassword from './pages/ForgotPassword.jsx';
-import ResetPassword from './pages/ResetPassword.jsx';
-import ProtectedRoute from './routes/ProtectedRoute.jsx';
-import PublicRoute from './routes/PublicRoute.jsx';
-import { AuthProvider } from './context/AuthContext.js';
-import { roles } from './constants/systemRoles.js';
-
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import UserDashboard from "./pages/UserDashboard.jsx";
+import Home from "./pages/Home.jsx";
+import { ToastContainer, Bounce } from "react-toastify";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import PublicRoute from "./routes/PublicRoute.jsx";
+import { AuthProvider } from "./context/AuthContext.js";
+import { roles } from "./constants/systemRoles.js";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
-   <AuthProvider>
-    <BrowserRouter>
-    <Routes>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* authentication routes , accesasable by puplic and not loggedin users */}
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Home></Home>
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="/sign-up"
+              element={
+                <PublicRoute>
+                  <SignUp></SignUp>
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login></Login>
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="/forgot-password"
+              element={
+                <PublicRoute>
+                  <ForgotPassword></ForgotPassword>
+                </PublicRoute>
+              }
+            ></Route>
+            <Route
+              path="/reset-password/:token"
+              element={
+                <PublicRoute>
+                  <ResetPassword></ResetPassword>
+                </PublicRoute>
+              }
+            ></Route>
 
-       {/* authentication routes , accesasable by puplic and not loggedin users */}
-      <Route path="/" element={<PublicRoute><Home></Home></PublicRoute>}></Route>
-      <Route path="/sign-up" element={<PublicRoute><SignUp></SignUp></PublicRoute>}></Route>
-      <Route path="/login" element={<PublicRoute><Login></Login></PublicRoute>}></Route>
-      <Route path="/forgot-password" element={<PublicRoute><ForgotPassword></ForgotPassword></PublicRoute>}></Route>
-      <Route path="/reset-password/:token" element={<PublicRoute><ResetPassword></ResetPassword></PublicRoute>}></Route>
+            {/* protected routes */}
+            <Route
+              path="/admin-dashboard"
+              element={
+                <ProtectedRoute allowedRoles={roles.admin}>
+                  <AdminDashboard></AdminDashboard>
+                </ProtectedRoute>
+              }
+            ></Route>
+            <Route
+              path="/user-dashboard"
+              element={
+                <ProtectedRoute allowedRoles={roles.user}>
+                  <UserDashboard></UserDashboard>
+                </ProtectedRoute>
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer //for async toast msgs
+          position="top-center"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
 
-
-{/* protected routes */}
-      <Route path="/admin-dashboard" element={<ProtectedRoute allowedRoles={roles.admin}><AdminDashboard></AdminDashboard></ProtectedRoute>}></Route>
-      <Route path="/user-dashboard" element={<ProtectedRoute allowedRoles={roles.user}><UserDashboard></UserDashboard></ProtectedRoute>}></Route>
-
-
-     </Routes>
-     </BrowserRouter>
-      <ToastContainer
-        position="top-center"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable={false}
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
+        <Toaster //for sync msgs
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toasterId="default"
+          duration="4000"
+        />
       </AuthProvider>
-      </>
+    </>
   );
 }
 
--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,19 +1,37 @@
 import { roles } from "../constants/systemRoles";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigationType } from "react-router-dom";
 
 //tp prevent logged-in users to visit such as pages which are wrapped with Puplic route
 function PublicRoute({ children }) {
   const { user } = useContext(AuthContext);
 
   if (!user) {
-    //prevenr him,so keep him in his current location , so only allow for not loggedin users
+    //prevenr him,so keep him in his current location , so only allow it for not loggedin users
     return children;
   }
 
+  const toastShown = useRef(false); // ✅ prevent multi toast msg appear
+
+  const navigationType = useNavigationType();
+
+  if (user && navigationType === "POP") {
+    if (!toastShown.current) {
+      toast.error("Sign out first !", {
+        style: {
+          width: "500px",
+        },
+      });
+
+      toastShown.current = true;
+    }
+  }
+
   // If logged in → redirect based on role
-  else if (user.role === roles.user)
+  if (user.role === roles.user)
     return <Navigate to="/user-dashboard" replace />;
   else if (user.role === roles.admin)
     return <Navigate to="/admin-dashboard" replace />;
