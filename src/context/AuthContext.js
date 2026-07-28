fe98f49518e87f4222a2fe16cf0f1f7879f1c80e
raya <khasaatirayaa@gmail.com>
2026-07-28 12:44:11 +0300

    fix manual deletion case from local storage + slowdown navigation to login in logout()

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { startTimer } from "../utils/tokenExpiry";
+import { Navigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
@@ -16,17 +17,51 @@ export const AuthProvider = ({ children }) => {
     }
   }, [user]);//so in every user update this will be triggered so refresh token 
 
+
+   // ✅ Detect manual localStorage changes (VERY IMPORTANT) such as deletion
+  useEffect(() => {
+    const handleStorageChange = () => {
+      const stored = localStorage.getItem("user");
+
+      if (!stored) {
+        logout();
+      } else {
+        setUser(JSON.parse(stored));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    //cleanup function
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  // ✅ Extra safety check (runs on every render)
+  useEffect(() => {
+    const stored = localStorage.getItem("user");
+
+    if (!stored && user) {
+      logout();
+    }
+  }, [user]);
+
+
   // ✅ LOGIN
   const login = (data) => {
     localStorage.setItem("user", JSON.stringify(data));
     setUser(data);
   };
 
+   
   // ✅ LOGOUT
   const logout = () => {
     localStorage.removeItem("user");
     setUser(null);
     window.location.replace("/login");//replace as replace prop we passed previosully
+   //navigate("/login",{replace:true});
+   return <Navigate to="/login"></Navigate>
   };
 
   return (
