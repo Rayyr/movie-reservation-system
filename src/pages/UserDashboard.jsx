1495ba66bbfba5c14a613b0849804549da22e324
raya <khasaatirayaa@gmail.com>
2026-07-29 01:20:27 +0300

    add bg for userdasboard/ + replace .css file by .mdule.css for styles isolation

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,7 +1,34 @@
-function UserDashboard(){
-    return <h1>weelcome user!</h1>
-};
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import styles from "../styles/userDashboard/userDashboard.module.css";
+import AuroraBG from "../components/built-in/Aurora-bg";
+ 
+function UserDashboard() {
+  const { user } = useContext(AuthContext);
 
-
+  return (
+    <div className={styles.main}>
+      <AuroraBG
+        colorStops={["#4299e1", "#000000", "#7B1025"]}
+        blend={0.9}
+        amplitude={2.2}
+        speed={1.6}
+      ></AuroraBG>
+      <h1>weelcome user! {user.username}</h1>
+      {/* Navbar */}
+      <nav className={styles.navbar}>
+        <div className={styles.navLinks}>
+          <Link to="/login" replace={false}>
+            Login
+          </Link>
+          <Link to="/sign-up" replace={false}>
+            Sign-up
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 export default UserDashboard;
